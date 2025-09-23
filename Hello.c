7a4f9b077714bc90cb40c419481d4eb0ac3fecf1
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2025-09-23 12:43:09 +0530

    DSAin C

--- Hello.c
+++ Hello.c
@@ -362,7 +362,132 @@ int main() {
     return 0;
 }
 
+// Linear Search in C 
+#include <stdio.h> 
+int main()
+{
+   int array[100], search, c, n, count = 0;
+   
+   printf("Enter number of elements in array\n");
+   scanf("%d", &n);
+   
+   printf("Enter %d numbers\n", n);
+   
+   for (c = 0; c < n; c++)
+      scanf("%d", &array[c]);
+     
+   printf("Enter a number to search\n");
+   scanf("%d", &search);
+   
+   for (c = 0; c < n; c++) {
+      if (array[c] == search) {
+         printf("%d is present at location %d.\n", search, c+1);
+         count++;
+      }
+   }
+   if (count == 0)
+      printf("%d isn't present in the array.\n", search);
+   else
+      printf("%d is present %d times in the array.\n", search,count);
+     
+   return 0;
+}
+
+
+// linked list in C (Single linked list)
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void printList(struct Node* n) {
+    while (n != NULL) {
+        printf("%d -> ", n->data);
+        n = n->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = NULL;
+
+    
+    printList(head);
+
+    
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+}
+
 */
+// double linked list in c 
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+void printList(struct Node*node ){
+    struct Node* last = NULL;
+    printf("Transverse in forward direction \n");
+    while (node != NULL){
+        printf(" %d \t", node->data);
+        last = node;
+        node = node->next;
+    }
+    printf("NULL\n");
+    printf("Transverse in reverse direction \n");
+    while (last != NULL){
+        printf(" %d \t", last->data);
+        last = last->prev;
+    }
+    printf("NULL\n");
+}
+int main(){
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+    head->prev = NULL;
+
+    second->data = 20;
+    second->next = third;
+    second->prev = head;
+
+    third->data = 30;
+    third->next = NULL;
+    third->prev = second;
+
+    printList(head);
+
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+}
+
+
 
 
 
