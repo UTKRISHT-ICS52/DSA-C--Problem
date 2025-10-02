13820c8ec5eefb46d67960af28e72096c58f57e3
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2025-10-03 00:13:01 +0530

    c language

--- Hello.c
+++ Hello.c
@@ -436,7 +436,7 @@ int main() {
     return 0;
 }
 
-*/
+
 // double linked list in c 
 #include <stdio.h>
 #include <stdlib.h>
@@ -488,8 +488,85 @@ int main(){
 }
 
 
+// Queue Implemention in C 
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100 
 
+typedef struct Queue {
+    int arr[MAX];
+    int front, rear;
+} Queue;
 
+void initQueue(Queue* q) {
+    q->front = -1;
+    q->rear = -1;
+}
 
+int isFull(Queue* q) {
+    return (q->rear + 1) % MAX == q->front;
+}
 
+int isEmpty(Queue* q) {
+    return q->front == -1;
+}
+
+void enqueue(Queue* q, int value) {
+    if (isFull(q)) {
+        printf("Queue is full\n");
+        return;
+    }
+    if (isEmpty(q)) {
+        q->front = 0;
+    }
+    q->rear = (q->rear + 1) % MAX;
+    q->arr[q->rear] = value;
+}
+
+int dequeue(Queue* q) {
+    if (isEmpty(q)) {
+        printf("Queue is empty\n");
+        return -1;
+    }
+    int value = q->arr[q->front];
+    if (q->front == q->rear) {
+        q->front = -1;
+        q->rear = -1;
+    } else {
+        q->front = (q->front + 1) % MAX;
+    }
+    return value;
+}
+
+*/
+// Linear Search in C 
+#include <stdio.h> 
+int main()
+{
+   int array[100], search, c, n, count = 0;
+   
+   printf("Enter number of elements in array\n");
+   scanf("%d", &n);
+   
+   printf("Enter %d numbers\n", n);
+   
+   for (c = 0; c < n; c++)
+      scanf("%d", &array[c]);
+     
+   printf("Enter a number to search\n");
+   scanf("%d", &search);
+   
+   for (c = 0; c < n; c++) {
+      if (array[c] == search) {
+         printf("%d is present at location %d.\n", search, c+1);
+         count++;
+      }
+   }
+   if (count == 0)
+      printf("%d isn't present in the array.\n", search);
+   else
+      printf("%d is present %d times in the array.\n", search,count);
+     
+   return 0;
+}
 
